25108068cd4233d9dafd24d72c2612ce8c5ec8f3
Pierpaolo Di Dato <pierpaolo.pdd@gmail.com>
2021-05-03 12:59:37 +0200

    Update

--- src/com/heroku/test/Test.java
+++ src/com/heroku/test/Test.java
@@ -3,7 +3,7 @@ package com.heroku.test;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.DeliverCallback;
 
 public class Test {
 
@@ -13,17 +13,27 @@ public class Test {
 
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setUri(uri);
-		factory.setRequestedHeartbeat(30);
-		factory.setConnectionTimeout(30);
+
 		Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
 
-		channel.queueDeclare("hello", false, false, false, null);
-		String message = "Hello CloudAMQP!!!";
-		channel.basicPublish("", "hello", null, message.getBytes());
+		String queue = "hello";     //queue name
+		boolean durable = true;    //durable - RabbitMQ will never lose the queue if a crash occurs
+		boolean exclusive = false;  //exclusive - if queue only will be used by one connection
+		boolean autoDelete = false; //autodelete - queue is deleted when last consumer unsubscribes
+
+		channel.queueDeclare(queue, durable, exclusive, autoDelete, null);
+		String message = "Hello CloudAMQP!";
+
+		String exchangeName = "";
+		String routingKey = "hello";
+		channel.basicPublish(exchangeName, routingKey, null, message.getBytes());
 		System.out.println(" [x] Sent '" + message + "'");
 
-		QueueingConsumer consumer = new QueueingConsumer(channel);
-		channel.basicConsume("hello", true, consumer);
+		DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+			String message1 = new String(delivery.getBody(), "UTF-8");
+			System.out.println(" [x] Received '" + message1 + "'");
+		};
+		channel.basicConsume(queue, true, deliverCallback, consumerTag -> { });
 	}
 }
